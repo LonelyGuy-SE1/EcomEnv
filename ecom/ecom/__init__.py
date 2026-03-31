95c84abbb9f0f5a6af54f73537b40834be8fb645
LonelyGuy-SE1 <brawlers.assemble@gmail.com>
2026-03-31 12:23:18 +0530

    initial commit

---
+++ ecom/ecom/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Ecom Environment."""
+
+from .client import EcomEnv
+from .models import EcomAction, EcomObservation
+
+__all__ = [
+    "EcomAction",
+    "EcomObservation",
+    "EcomEnv",
+]
---
+++ ecom/ecom/server/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Ecom environment server components."""
+
+from .ecom_environment import EcomEnvironment
+
+__all__ = ["EcomEnvironment"]
